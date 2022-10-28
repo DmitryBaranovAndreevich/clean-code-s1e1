48420a355813e72b8f9f76e8b87d609a100b39e5
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2022-10-28 16:43:13 +0300

    fix: delete teags selectors and add class selectors according to rule  3.4

--- app.js
+++ app.js
@@ -32,19 +32,22 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = 'list';
     label.innerText=taskString;
-    label.className='task';
+    label.className='task label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className = "input task text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className = "edit button";
 
-    deleteButton.className="delete";
+    deleteButton.className = "delete button";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "delete__image";
     deleteButton.appendChild(deleteButtonImg);
 
 
